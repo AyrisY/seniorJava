528bf9c7169a3eed1bc8d5e0698499d78ae1a1d7
yangjie <yangjie@nonobank.com>
2017-08-09 14:23:45 +0800

    动态代理注释说明

--- src/main/java/proxy/ProxyTest.java
+++ src/main/java/proxy/ProxyTest.java
@@ -7,8 +7,10 @@ import org.junit.Test;
  */
 public class ProxyTest {
 
+
     @Test
     public void TestJDKProxy(){
+        //接口生成的类,JDK动态代理原理
         TravelProxy proxy=new TravelProxy();
         Travel travel=(Travel)proxy.blind(new TravelImpl());
 
@@ -17,6 +19,7 @@ public class ProxyTest {
 
     @Test
     public void TestCglib(){
+        //cglib可以不依赖接口,直接根据类实体生成代理类
         TravelProxyCglib proxyCglib=new TravelProxyCglib();
         TravelImpl2 travel=(TravelImpl2) proxyCglib.getInstance(new TravelImpl2());
 

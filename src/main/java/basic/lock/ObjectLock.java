58da7f59ff76ba824a621ceec2a645453b9f0b00
yangjie <yangjie@nonobank.com>
2019-11-12 18:10:12 +0800

    锁理解

---
+++ src/main/java/basic/lock/ObjectLock.java
@@ -0,0 +1,45 @@
+package basic.lock;
+
+/**
+ * @author yangjie
+ * @date 2019-11-12
+ * @time 17:56
+ */
+public class ObjectLock {
+
+
+    public synchronized void print1() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("print1---" + i);
+        }
+    }
+
+    public synchronized void print2() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("print2---" + i);
+        }
+    }
+
+    public static void main(String[] args) {
+        ObjectLock objectLock = new ObjectLock();
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                objectLock.print1();
+            }
+        });
+
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                objectLock.print2();
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+    }
+
+}
---
+++ src/main/java/basic/lock/StaticLock.java
@@ -0,0 +1,9 @@
+package basic.lock;
+
+/**
+ * @author yangjie
+ * @date 2019-11-12
+ * @time 17:56
+ */
+public class StaticLock {
+}
